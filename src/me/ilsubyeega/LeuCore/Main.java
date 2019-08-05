1e9d5a8ccdeb43cce39aab00de65f153c5133fdc
travis-ci <travis@travis-ci.org>
2019-08-05 15:45:13 +0900

    Edited something, but not supporting vault now

--- src/me/ilsubyeega/LeuCore/Main.java
+++ src/me/ilsubyeega/LeuCore/Main.java
@@ -5,27 +5,20 @@ import me.ilsubyeega.LeuCore.commands.MoneyCommand;
 import me.ilsubyeega.LeuCore.events.AntiCropJumping;
 import me.ilsubyeega.LeuCore.events.JoinQuitEvent;
 import me.ilsubyeega.LeuCore.events.MOTDEvent;
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import net.milkbowl.vault.chat.Chat;
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.economy.EconomyResponse;
-import net.milkbowl.vault.permission.Permission;
-
-import java.util.logging.Logger;
 
 
 public class Main extends JavaPlugin {
     private static Economy econ = null;
-    private static Permission perms = null;
+    public static Permission perms = null;
     private static Chat chat = null;
 
-
+    @Override
     public void onEnable() {
         // Vault 플러그인이 있는지 확인합니다.
         if (!setupEconomy() ) {
@@ -33,8 +26,16 @@ public class Main extends JavaPlugin {
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-        setupPermissions();
-        setupChat();
+        if (!setupPermissions()) {
+            System.out.println(Config.prefix + "Vault 플러그인(또는 관련 플러그인)이 없습니다! Vault 플러그인을 넣은 후 실행해주세요.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        if (!setupChat()) {
+            System.out.println(Config.prefix + "Vault 플러그인(또는 관련 플러그인)이 없습니다! Vault 플러그인을 넣은 후 실행해주세요.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
         // 이벤트 추가
         getServer().getPluginManager().registerEvents(new JoinQuitEvent(), this);
@@ -55,25 +56,6 @@ public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         System.out.println(Config.prefix + "§cLeuCore가 비활성화 되었습니다. §8[ilsubyeega]");
-    }
-
-    public class TestCommand implements CommandExecutor {
-
-        // This method is called, when somebody uses our command
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            Bukkit.broadcastMessage(Config.prefix + "§7잘 입력했어 ㅅㅅ");
-            return true;
-        }
-    }
-    public class TestCommand2 implements CommandExecutor {
-
-        // This method is called, when somebody uses our command
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            Bukkit.broadcastMessage(Config.prefix + "§7잘 입력했어 ㅅㅅ2");
-            return true;
-        }
     }
 
 
@@ -98,19 +80,19 @@ public class Main extends JavaPlugin {
         if (rsp == null) {
             return false;
         }
-        econ = rsp.getProvider();
+        Economy econ = rsp.getProvider();
         return econ != null;
     }
 
     private boolean setupChat() {
         RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
-        chat = rsp.getProvider();
+        Chat chat = rsp.getProvider();
         return chat != null;
     }
 
     private boolean setupPermissions() {
         RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
-        perms = rsp.getProvider();
+        Permission perms = rsp.getProvider();
         return perms != null;
     }
 
--- src/me/ilsubyeega/LeuCore/commands/MoneyCommand.java
+++ src/me/ilsubyeega/LeuCore/commands/MoneyCommand.java
@@ -1,16 +1,19 @@
 package me.ilsubyeega.LeuCore.commands;
 
-import me.ilsubyeega.LeuCore.Main;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import me.ilsubyeega.LeuCore.Config;
-
+import me.ilsubyeega.LeuCore.Main;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import net.milkbowl.vault.permission.Permission;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
 
 import java.util.Arrays;
 
@@ -18,9 +21,8 @@ public class MoneyCommand implements CommandExecutor {
     // 명령어 실행 (/money)
     // Vault hook
     private static Economy econ = null;
-    private static Permission perms = null;
-    private static Economy getEconomy() {        return econ;    }
-    private static Permission getPermissions() {        return perms;    }
+    private static Permission perms = Main.perms;
+    private static Chat chat = null;
     // 명령어 부문
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -28,25 +30,28 @@ public class MoneyCommand implements CommandExecutor {
         if (args.length == 0) {
             sender.sendMessage(Config.moneyprefix + "§7사용 방법: §e/돈 도움말");
         } else if (args[0].equalsIgnoreCase("help")) {
-            if (getPermissions().has(sender, "leu.money.help")) {
+            if (perms.has(sender, "leu.money.help")) {
                 sender.sendMessage(Config.moneyprefix + "§7돈 도움말");
             } else {
                 sender.sendMessage(Config.moneyprefix + "§c당신은 권한이 부족합니다! [ leu.money.help ]");
             }
         } else if (args[0].equalsIgnoreCase("view")) {
-                if (args[1].length() == 0){
+                Player player = (Player) sender;
+                if (args.length == 1){
                     sender.sendMessage("Beeeee");
-                    if (getPermissions().has(sender, "leu.money.view")) {
-                        sender.sendMessage(Config.moneyprefix + "§7당신의 돈은 §c"+ getEconomy().format(getEconomy().getBalance((OfflinePlayer) sender)) + "§7입니다.");
+                    if (perms.has(player, "leu.money.view")) {
+                        sender.sendMessage("ㅎㅇ");
+                        sender.sendMessage(Config.moneyprefix + "§7당신의 돈은 §c"+ "0원" + "§7입니다.");
                     } else {
                         sender.sendMessage(Config.moneyprefix + "§c당신은 권한이 부족합니다! [ leu.money.view ]");
                     }
-            } else {
-            sender.sendMessage(Config.moneyprefix + "§c해당 기능은 준비중입니다.");
-            }
+                } else {
+                        sender.sendMessage(args[1]);
+                        sender.sendMessage(Config.moneyprefix + "§c해당 기능은 준비중입니다.");
+                }
         } else if (args[0].equalsIgnoreCase("admin")) {
-            if(getPermissions().has(sender, "leu.money.admin")) {
-
+            if (perms.has(sender, "leu.money.admin")) {
+                sender.sendMessage("ㅎㅇ");
         }
 
         } else {
@@ -54,5 +59,6 @@ public class MoneyCommand implements CommandExecutor {
         }
         return true;
     }
+
 
 }
